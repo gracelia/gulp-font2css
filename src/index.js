c95b3a8585a99cc90a6105ff4b9026c367a1c269
gracelia <darlyn@foxmail.com>
2021-10-29 20:07:49 +0800

    chore: regx cover filename with Chinese

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import PluginError from 'plugin-error'
  */
 function customFilePath(file, dest) {
     if (!file) return
-    const filename = /([\w.-\s]+).\w+$/.exec(file.path)[0];
+    const filename = /.+\/(.+.[otf|ttf|woff|woff2])$/.exec(file.path)[1];
     return /\/$/.test(dest) ? dest + filename : '/' + dest + filename;
 }
 
